a15a679c06cbbae1cce5d94169fb2964e5899eba
ParkGana <gnchoco97@gmail.com>
2025-07-08 10:27:19 +0900

    로컬에서 IP 주소로 접속 가능하도록 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // 로컬에서 IP 주소로 접속 가능하도록 설정
+  server: {
+    host: '0.0.0.0'
+  }
 });
