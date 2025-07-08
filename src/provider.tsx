9ea2de5c61729ad36049eede6351ff5cd6b25cd8
ParkGana <gnchoco97@gmail.com>
2025-07-08 15:03:13 +0900

    TanStack Query - 설치 및 설정

--- src/provider.tsx
+++ src/provider.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components';
 import AuthProvider from './providers/authProvider';
 import { darkTheme, theme as lightTheme } from './styles/theme';
 import { useThemeStore } from './stores/themeStore';
+import QueryProvider from './providers/queryProvider';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   const { theme } = useThemeStore();
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <AuthProvider>{children}</AuthProvider>
+      <QueryProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </QueryProvider>
     </ThemeProvider>
   );
 };
