54c454eaff6670071743f166e04e8ac81d4a6880
ParkGana <gnchoco97@gmail.com>
2025-07-08 10:31:27 +0900

    React Router DOM - 기본 사용법

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
+
       {/* 등록되지 않은 경로로 접속할 경우 / 경로로 redirect */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
