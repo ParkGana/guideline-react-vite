21382b2a50600a8377fad03370bea6bcea5ad997
ParkGana <gnchoco97@gmail.com>
2025-07-08 14:33:25 +0900

    Styled Components - 다크모드 설정

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import AuthProvider from './providers/authProvider';
-import { theme } from './styles/theme';
+import { darkTheme, theme as lightTheme } from './styles/theme';
+import { useThemeStore } from './stores/themeStore';
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { theme } = useThemeStore();
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
       <AuthProvider>{children}</AuthProvider>
     </ThemeProvider>
   );
