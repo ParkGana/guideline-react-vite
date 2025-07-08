3197fc03543911ad2aa658e8946d378627b40251
ParkGana <gnchoco97@gmail.com>
2025-07-08 10:41:55 +0900

    Provider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App.tsx';
+import Provider from './provider.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <Provider>
+    <App />
+  </Provider>
+);
