2b47f6864f3444f447be6ac8dc4bbe0f096d243a
ParkGana <gnchoco97@gmail.com>
2025-07-09 17:40:37 +0900

    TanStack Query - useMutation - 낙관적 업데이트 (Todo 예제)

--- src/hooks/query/useTodo.ts
+++ src/hooks/query/useTodo.ts
@@ -1,5 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { createTodoAPI, deleteTodoAPI, fetchTodosAPI, updateTodoAPI } from '../../api/Todo';
+import type { TodoType } from '../../types/todoType';
+import { v4 as uuidv4 } from 'uuid';
 
 export const useTodo = () => {
   const queryClient = useQueryClient();
@@ -13,7 +15,25 @@ export const useTodo = () => {
   /* Todo 생성 */
   const createTodoMutation = useMutation({
     mutationFn: createTodoAPI,
-    onSuccess: () => {
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['todos'] });
+    // }
+    onMutate: async (title: string) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] });
+
+      const prevTodos = queryClient.getQueryData(['todos']);
+
+      queryClient.setQueryData<TodoType[]>(['todos'], (prev = []) => [
+        ...prev,
+        { id: uuidv4(), title, completed: false }
+      ]);
+
+      return { prevTodos };
+    },
+    onError: (_err, _variables, context) => {
+      context && queryClient.setQueryData(['todos'], context.prevTodos);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
     }
   });
@@ -21,7 +41,24 @@ export const useTodo = () => {
   /* Todo 수정 */
   const updateTodoMutation = useMutation({
     mutationFn: updateTodoAPI,
-    onSuccess: () => {
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['todos'] });
+    // }
+    onMutate: async ({ id, completed }) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] });
+
+      const prevTodos = queryClient.getQueryData(['todos']);
+
+      queryClient.setQueryData<TodoType[]>(['todos'], (prev = []) =>
+        prev.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+      );
+
+      return { prevTodos };
+    },
+    onError: (_err, _variables, context) => {
+      context && queryClient.setQueryData(['todos'], context.prevTodos);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
     }
   });
@@ -29,7 +66,22 @@ export const useTodo = () => {
   /* Todo 삭제 */
   const deleteTodoMutation = useMutation({
     mutationFn: deleteTodoAPI,
-    onSuccess: () => {
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['todos'] });
+    // }
+    onMutate: async (id) => {
+      await queryClient.cancelQueries({ queryKey: ['todos'] });
+
+      const prevTodos = queryClient.getQueryData(['todos']);
+
+      queryClient.setQueryData<TodoType[]>(['todos'], (prev = []) => prev.filter((todo) => todo.id !== id));
+
+      return { prevTodos };
+    },
+    onError: (_err, _variables, context) => {
+      context && queryClient.setQueryData(['todos'], context.prevTodos);
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ['todos'] });
     }
   });
