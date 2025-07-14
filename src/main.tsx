ec88a3096d58932623e35ae18d9beb900f99fb86
ParkGana <gnchoco97@gmail.com>
2025-07-14 16:08:09 +0900

    Redux - 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import './styles/index.css';
 import './styles/tailwind.css';
 import App from './App.tsx';
 import Provider from './provider.tsx';
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <Provider>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </Provider>
 );
